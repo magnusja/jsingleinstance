c3e545fac15c0a5d03343a89ac106286292a6792
mjdv <mjdv@localhost>
2011-05-31 16:13:25 +0200

    few enhancements

--- src/test/Test.java
+++ src/test/Test.java
@@ -3,6 +3,8 @@ package test;
 import java.io.IOException;
 
 import jsingleinstace.JSingleInstance;
+import jsingleinstace.JSingleInstance.CommandEvent;
+import jsingleinstace.JSingleInstance.CommandListener;
 
 public class Test {
 
@@ -12,6 +14,14 @@ public class Test {
 	public static void main(String[] args) {
 		try {
 			JSingleInstance i = new JSingleInstance("/tmp/jsingle");
+			i.addDataEventListener(new CommandListener() {
+
+				@Override
+				public void onCommand(CommandEvent e) {
+					
+				}
+				
+			});
 			System.out.println(i.sendCommand("test123"));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
