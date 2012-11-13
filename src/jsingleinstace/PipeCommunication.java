279f4b1d7f2cac2055a17a8f4ea852213bc3e6fd
Magnus <magnusja@users.noreply.github.com>
2012-11-13 14:34:33 +0100

    Forgot to add some files

---
+++ src/jsingleinstace/PipeCommunication.java
@@ -0,0 +1,36 @@
+package jsingleinstace;
+
+
+public class PipeCommunication extends Communication {
+
+	@Override
+	void init() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	boolean isAlreadyRunning() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	boolean sendCommand(String cmd) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	String waitForCommand() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	void shutdown() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
